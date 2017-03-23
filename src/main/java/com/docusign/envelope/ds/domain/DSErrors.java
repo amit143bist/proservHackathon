53d37ffc2d914facea0b90348842d964d80e6e1d
Amit.Bist <Amit.Bist@DSW004160.docusignhq.com>
2017-03-23 03:41:43 -0500

    Error

---
+++ src/main/java/com/docusign/envelope/ds/domain/DSErrors.java
@@ -0,0 +1,36 @@
+package com.docusign.envelope.ds.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "errorCode", "message" })
+public class DSErrors {
+
+	@JsonProperty("errorCode")
+	private String errorCode;
+	@JsonProperty("message")
+	private String message;
+
+	@JsonProperty("errorCode")
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	@JsonProperty("errorCode")
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	@JsonProperty("message")
+	public String getMessage() {
+		return message;
+	}
+
+	@JsonProperty("message")
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
